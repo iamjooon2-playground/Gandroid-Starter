e5c282cb7f2f520dbb2f20117f19187403b1a71f
iamjooon2 <iamjooon2@gmail.com>
2024-04-18 15:30:47 +0900

    코틀린으로 만든 나의 버튼

--- app/src/main/java/com/example/gandroid/MainActivity.kt
+++ app/src/main/java/com/example/gandroid/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.gandroid
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        // LinearLayout 설정
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+
+        val baseLayout = LinearLayout(this)
+
+        baseLayout.orientation = LinearLayout.VERTICAL
+        baseLayout.setBackgroundColor(Color.rgb(0, 255, 0))
+        setContentView(baseLayout, params)
+
+        var button = Button(this)
+        button.text = "버튼입니다"
+        button.setBackgroundColor(Color.MAGENTA)
+        baseLayout.addView(button)
+
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "코드로 생성한 버튼입니다 데헷", Toast.LENGTH_SHORT).show()
+        }
 
         showActionBar()
     }
