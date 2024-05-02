99759ede8d12604c3b83ae5cf4531048865f2c68
iamjooon2 <iamjooon2@gmail.com>
2024-05-02 19:58:47 +0900

    WIP

--- app/src/main/java/com/example/gandroid/MainActivity.kt
+++ app/src/main/java/com/example/gandroid/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.gandroid
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ViewFlipper
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var buttonPreview: Button
+        var buttonNext: Button
+        var flipper: ViewFlipper
+
+        buttonPreview = findViewById(R.id.buttonPreview)
+        buttonNext = findViewById(R.id.buttonNext)
+        flipper = findViewById(R.id.viewFlipper)
+
+        buttonPreview.setOnClickListener {
+            flipper.showPrevious()
+        }
+
+        buttonNext.setOnClickListener {
+            flipper.showNext()
+        }
 
         showActionBar()
     }
