65bc288eda9262df48f917b5907055c4340a9d4d
iamjooon2 <iamjooon2@gmail.com>
2024-05-16 15:53:36 +0900

    토스트 연습

--- app/src/main/java/com/example/gandroid/MainActivity.kt
+++ app/src/main/java/com/example/gandroid/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.gandroid
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = "토스트 연습"
+
+        var button = findViewById<Button>(R.id.button1)
+
+        button.setOnClickListener {
+            var toastMessage = Toast.makeText(applicationContext, "토스트 연습", Toast.LENGTH_SHORT)
+
+            var display = (getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay
+
+            var xOffSet = (Math.random() * display.width).toInt()
+            var yOffSet = (Math.random() * display.height).toInt()
+
+            toastMessage.setGravity(Gravity.TOP or Gravity.LEFT, xOffSet, yOffSet)
+            toastMessage.show()
+        }
 
         showActionBar()
     }
