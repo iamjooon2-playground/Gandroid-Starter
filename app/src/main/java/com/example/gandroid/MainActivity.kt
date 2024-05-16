f6b77df837dd9b0675a9fda6c15b5c92041bfb64
iamjooon2 <iamjooon2@gmail.com>
2024-05-16 14:09:55 +0900

    웹뷰 프로그램 만들기

--- app/src/main/java/com/example/gandroid/MainActivity.kt
+++ app/src/main/java/com/example/gandroid/MainActivity.kt
@@ -1,19 +1,54 @@
 package com.example.gandroid
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit internal var editUrl: EditText
+    lateinit internal var go: Button
+    lateinit internal var back: Button
+    lateinit internal var web: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editUrl = findViewById(R.id.editUrl)
+        go = findViewById(R.id.go)
+        back = findViewById(R.id.back)
+        web = findViewById(R.id.webView)
+
+        web.webViewClient = JunHeeWebView()
+
+        var webSet = web.settings
+        webSet.builtInZoomControls = true
+
+        go.setOnClickListener {
+            web.loadUrl(editUrl.text.toString())
+        }
+
+        back.setOnClickListener {
+            web.goBack()
+        }
 
         showActionBar()
     }
 
     private fun showActionBar() {
         supportActionBar?.setDisplayShowHomeEnabled(true)
-        supportActionBar?.setIcon(R.drawable.ic_launcher_background)
-        supportActionBar?.setIcon(R.drawable.ic_launcher_foreground)
+        supportActionBar?.setIcon(R.drawable.gachon)
     }
+
+    @Deprecated("deprecated")
+    class JunHeeWebView : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            return super.shouldOverrideUrlLoading(view, url)
+        }
+    }
+
 }
