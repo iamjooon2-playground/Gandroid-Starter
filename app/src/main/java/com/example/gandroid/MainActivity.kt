cd802c22df6f04d5208d4a4d51b25f3cc9b24ee2
iamjooon2 <iamjooon2@gmail.com>
2024-05-16 17:55:01 +0900

    temp

--- app/src/main/java/com/example/gandroid/MainActivity.kt
+++ app/src/main/java/com/example/gandroid/MainActivity.kt
@@ -1,9 +1,21 @@
 package com.example.gandroid
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tvName : TextView;
+    lateinit var tvEmail : TextView;
+    lateinit var button : TextView;
+    lateinit var dialogEditName : TextView;
+    lateinit var dialogEditText : TextView;
+    lateinit var toastText : TextView;
+    lateinit var dialogView : View;
+    lateinit var toastView : View;
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
