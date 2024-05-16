511fd5856010a975af312c2801b3be465f860af6
iamjooon2 <iamjooon2@gmail.com>
2024-05-16 21:04:16 +0900

    과제 완성

--- app/src/main/java/com/example/gandroid/MainActivity.kt
+++ app/src/main/java/com/example/gandroid/MainActivity.kt
@@ -2,23 +2,64 @@ package com.example.gandroid
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var tvName : TextView;
-    lateinit var tvEmail : TextView;
-    lateinit var button : TextView;
-    lateinit var dialogEditName : TextView;
-    lateinit var dialogEditText : TextView;
-    lateinit var toastText : TextView;
-    lateinit var dialogView : View;
-    lateinit var toastView : View;
-    
+    private val SUBMIT = "확인"
+    private val CANCEL = "취소"
+
+    lateinit var textViewName: TextView;
+    lateinit var textViewEmail: TextView;
+    lateinit var button: TextView;
+    lateinit var dialogEditName: EditText;
+    lateinit var dialogEditEmail: EditText;
+    lateinit var toastText: TextView;
+    lateinit var dialogView: View;
+    lateinit var toastView: View;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "이준희의 사용자 정보 입력"
+
+        textViewName = findViewById(R.id.tvName)
+        textViewEmail = findViewById(R.id.tvEmail)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            dialogView = View.inflate(this@MainActivity, R.layout.dialog, null)
+            var dialog = AlertDialog.Builder(this@MainActivity)
+
+            dialog.setTitle("사용자 정보 입력")
+            dialog.setIcon(R.drawable.people)
+            dialog.setView(dialogView)
+
+            dialog.setPositiveButton(SUBMIT) { dialog, index ->
+                dialogEditName = dialogView.findViewById<EditText>(R.id.dialogEdit1)
+                dialogEditEmail = dialogView.findViewById<EditText>(R.id.dialogEdit2)
+                textViewName.text = dialogEditName.text.toString()
+                textViewEmail.text = dialogEditEmail.text.toString()
+            }
+
+            dialog.setNegativeButton(CANCEL) { dialog, index ->
+                var toast = Toast(this@MainActivity)
+                toastView = View.inflate(this@MainActivity, R.layout.toast, null)
+
+                toastText = toastView.findViewById(R.id.toastText)
+                toastText.text = "취소했습니다"
+
+                toast.view = toastView
+
+                toast.show()
+            }
+
+            dialog.show()
+        }
 
         showActionBar()
     }
