2e9553592471d3afc2e41781de2db555a70af14c
iamjooon2 <iamjooon2@gmail.com>
2024-05-23 15:21:36 +0900

    내장 메모리 파일 처리

--- app/src/main/java/com/example/gandroid/MainActivity.kt
+++ app/src/main/java/com/example/gandroid/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.gandroid
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
 import android.widget.DatePicker
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import java.io.FileInputStream
+import java.io.IOException
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var datePicker : DatePicker
-    lateinit var editDiary: EditText
+    lateinit var editText: EditText
     lateinit var writeButton : Button
     lateinit var fileName : String
 
@@ -21,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         title = "이준희의 간단 일기장"
 
         datePicker = findViewById(R.id.datePicker)
-        editDiary = findViewById(R.id.editDiary)
+        editText = findViewById(R.id.editDiary)
         writeButton = findViewById(R.id.buttonWrite)
 
         var cal = Calendar.getInstance()
@@ -33,16 +36,40 @@ class MainActivity : AppCompatActivity() {
             // 달도 0달부터 세나?
             fileName = Integer.toString(year) + "_" + Integer.toString(monthOfYear + 1) + "_" + Integer.toString(dayOfMonth) + ".txt"
             var str = readDiary(fileName)
-            editDiary.setText(str)
-            writeButton.isEnabled = true
+            editText.setTextColor(t)
+        }
+
+        writeButton.setOnClickListener {
+            var outFs = openFileOutput(fileName, Context.MODE_PRIVATE)
+            var str = writeButton.text.toString()
+
+            outFs.write(str.toByteArray())
+            outFs.close()
+
+            // 토스트 메시지 띄우기
         }
 
         showActionBar()
     }
 
     private fun readDiary(fileName : String) : String ? {
-        return null;
+        var diaryString : String? = null
+        var inFs : FileInputStream
+
+        try {
+            inFs = openFileInput(fileName)
+            var txt = ByteArray(500)
+            inFs.read(txt)
+            inFs.close()
+            diaryString = txt.toString(Charsets.UTF_8).trim()
+        } catch (e : IOException) {
+            editText.hint = "일기 없음"
+            writeButton.text = "새로 저장"
+        }
+        return diaryString;
     }
+
+
 
     private fun showActionBar() {
         supportActionBar?.setDisplayShowHomeEnabled(true)
