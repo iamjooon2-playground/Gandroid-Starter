61cbfcd1977a7cbc9a0833e789e581f9ac6bbc69
iamjooon2 <iamjooon2@gmail.com>
2024-05-29 00:51:46 +0900

    feat: 내장 메모리 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.gandroid"
-        minSdk = 24
+        minSdk = 18
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -42,7 +42,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- app/src/main/java/com/example/gandroid/MainActivity.kt
+++ app/src/main/java/com/example/gandroid/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.example.gandroid
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.io.File
+import java.io.FileInputStream
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            Context.MODE_PRIVATE
+        )
+
+        var readButton = findViewById<Button>(R.id.readButton)
+        var editSD = findViewById<EditText>(R.id.editSd)
+
+        var makeButton = findViewById<Button>(R.id.makeButton)
+        var deleteButton = findViewById<Button>(R.id.deleteButton)
+
+        var strSDpath = Environment.getExternalStorageDirectory().absolutePath
+        var myDirectory = File("$strSDpath/mydir")
+
+        readButton.setOnClickListener {
+            var inFs = FileInputStream("/storage/emulated/0/sd_test.txt")
+            var txt = ByteArray(inFs.available())
+            inFs.read(txt)
+            editSD.setText(txt.toString(Charsets.UTF_8))
+            inFs.close()
+        }
+
+        makeButton.setOnClickListener {
+            myDirectory.mkdir()
+        }
+
+        deleteButton.setOnClickListener {
+            myDirectory.delete()
+        }
 
         showActionBar()
     }
