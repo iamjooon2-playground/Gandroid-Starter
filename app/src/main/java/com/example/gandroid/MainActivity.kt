b3c444b40f2107dc17f9b12a628bda27798f803a
iamjooon2 <iamjooon2@gmail.com>
2024-05-30 15:22:21 +0900

    355p

--- app/src/main/java/com/example/gandroid/MainActivity.kt
+++ app/src/main/java/com/example/gandroid/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.gandroid
 
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +19,18 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setIcon(R.drawable.ic_launcher_background)
         supportActionBar?.setIcon(R.drawable.ic_launcher_foreground)
+    }
+}
+
+private class MyGraphicView(context: Context) : View(context) {
+
+    override fun onDraw(canvas : Canvas) {
+        super.onDraw(canvas)
+
+        var picture = BitmapFactory.decodeResource(resources, R.drawable.gachon)
+        var picX = (this.width - picture.width) / 2f
+        var picY = (this.height - picture.height) / 2f
+        canvas.drawBitmap(picture, picX, picY, null)
+        picture.recycle()
     }
 }
