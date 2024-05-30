22bde3bfea55f0a650bb56f1e052874c06d9782b
iamjooon2 <iamjooon2@gmail.com>
2024-05-30 16:04:41 +0900

    예제 9-9

--- app/src/main/java/com/example/gandroid/MainActivity.kt
+++ app/src/main/java/com/example/gandroid/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.gandroid
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.graphics.BlurMaskFilter
 import android.graphics.Canvas
+import android.graphics.EmbossMaskFilter
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +13,9 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(MyGraphicView(this))
+//        setContentView(R.layout.activity_main)
+        title = "이준희의 비트맵 사용하기~"
 
         showActionBar()
     }
@@ -24,13 +29,43 @@ class MainActivity : AppCompatActivity() {
 
 private class MyGraphicView(context: Context) : View(context) {
 
-    override fun onDraw(canvas : Canvas) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        var picture = BitmapFactory.decodeResource(resources, R.drawable.gilya)
 
-        var picture = BitmapFactory.decodeResource(resources, R.drawable.gachon)
+        var cenX = this.width / 2f
+        var cenY = this.height / 2f
         var picX = (this.width - picture.width) / 2f
         var picY = (this.height - picture.height) / 2f
-        canvas.drawBitmap(picture, picX, picY, null)
+
+        var paint = Paint()
+        var blurMaskFilter = BlurMaskFilter(30f, BlurMaskFilter.Blur.NORMAL)
+        paint.maskFilter = blurMaskFilter
+//        var eMask: EmbossMaskFilter
+//        eMask = EmbossMaskFilter(floatArrayOf(3f, 3f, 3f), 0.5f, 5f, 10f)
+
+//        paint.maskFilter = eMask
+//        canvas.drawCircle(cenX, cenY, 150f, paint)
+//        canvas.drawBitmap(picture, picX, picY, paint)
+
         picture.recycle()
+
+
+//        canvas.drawBitmap(picture, picX, picY, null)
+//        picture.recycle()
+//
+//        canvas.rotate(45f, cenX, cenY)
+//        canvas.drawBitmap(picture, picX, picY, null)
+//
+//        canvas.translate(-150f, 200f)
+//        canvas.drawBitmap(picture, picX, picY, null)
+//
+//        canvas.scale(2f, 2f, cenX, cenY)
+//        canvas.drawBitmap(picture, picX, picY, null)
+//
+//        canvas.skew(0.3f, 0.3f)
+//        canvas.drawBitmap(picture, picX, picY, null)
+//
+//        picture.recycle()
     }
 }
